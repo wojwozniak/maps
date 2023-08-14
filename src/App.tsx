e3a0e167276dec0f2482e8cc95220e1563305a52
Wojtek <pulchnymamut@gmail.com>
2023-08-14 16:14:51 +0200

    scale map

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './index.css'
 
 function App() {
   return (
-    <div className='app'>
+    <div className='App'>
       <Map />
     </div>
   )
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,18 +1,20 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import * as d3 from 'd3';
 
-function Map() {
+const Map:React.FC = () => {
   const svgRef = useRef(null);
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+  const [svgWidth, setSvgWidth] = useState(viewportWidth * 0.6);
+  const [svgHeight, setSvgHeight] = useState((viewportWidth * 0.6 * 8) / 13);
 
   useEffect(() => {
     const svg = d3.select(svgRef.current);
-    const width = +svg.attr('width');
-    const height = +svg.attr('height');
+    svg.attr('viewBox', `0 0 ${svgWidth} ${svgHeight}`);
 
     const projection = d3.geoMercator()
-      .scale(12000)
+      .scale(10000)
       .center([8.2310, 46.8182])
-      .translate([width / 2, height / 2]);
+      .translate([svgWidth / 2, svgHeight / 2]);
 
     const pathGenerator = d3.geoPath().projection(projection);
 
@@ -22,22 +24,37 @@ function Map() {
         .data(topo.features)
         .enter()
         .append('path')
-        // Some weird error here, idk how to fix it
         // @ts-ignore
         .attr('d', pathGenerator)
-        .style('fill', 'blue');
+        .style('fill', 'green');
     };
 
     d3.json('https://raw.githubusercontent.com/mikpan/ch-maps/master/geo/ch-districts.geojson')
-      .then(ready)
-      .catch((error) => {
-        throw error;
-      });
+        .then(ready)
+        .catch((error) => {
+            throw error;
+        });
+
+
+    // ### Resize ###
+    function handleResize() {
+        const newViewportWidth = window.innerWidth;
+        setViewportWidth(newViewportWidth);
+        setSvgWidth(newViewportWidth * 0.8);
+        setSvgHeight((newViewportWidth * 0.8 * 8) / 13);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+    window.removeEventListener('resize', handleResize);
+    };
+    // ### End of Resize ###
 
   }, []);
 
   return (
-    <svg ref={svgRef} id="my_dataviz" width="1300" height="800" />
+    <svg ref={svgRef} id="my_dataviz" />
   );
 }
 
