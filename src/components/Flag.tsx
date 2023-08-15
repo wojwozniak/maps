62a5c78c53efffd9284fc55f9b52ae796d69ff1a
Wojtek <pulchnymamut@gmail.com>
2023-08-15 16:59:02 +0200

    import flag component

---
+++ src/components/Flag.tsx
@@ -0,0 +1,23 @@
+import { US, PL, DE } from 'country-flag-icons/react/3x2'
+
+type FlagMapType = {
+    [key: string]: React.ReactElement;
+};
+
+const Flag = ({ currentLang }: { currentLang: string }) => {
+    const flagStyle = {
+        height: '20px',
+        width: '30px',
+    }
+
+    const flagMap: FlagMapType = {
+        en: <US title="United States of America" style={flagStyle} />,
+        pl: <PL title="Poland" style={flagStyle} />,
+        de: <DE title="Germany" style={flagStyle} />
+    };
+
+    const flagToRender = flagMap[currentLang] || null;
+    return flagToRender;
+};
+
+export default Flag
