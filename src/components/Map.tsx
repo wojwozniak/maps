da4912fcd34860d7171879dc99d8c9e1578bf33f
Wojtek <pulchnymamut@gmail.com>
2023-08-18 19:44:11 +0200

    restructure and comment

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,18 +3,22 @@ import * as d3 from 'd3';
 
 const Map:React.FC = () => {
   const svgRef = useRef(null);
+
+  /* ### Scaling ### */
   const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
   const [svgWidth, setSvgWidth] = useState(viewportWidth * .4);
   const [svgHeight, setSvgHeight] = useState((viewportWidth * .4 * 2) / 3);
-
   const scaleFactor = 11;
   const [scale, updateScale] = useState(svgWidth * scaleFactor);
+  /* ### End of scaling ### */
 
   useEffect(() => {
     const svg = d3.select(svgRef.current);
     svg.attr('viewBox', `0 0 ${svgWidth} ${svgHeight}`);
     svg.style('background-color', '#FEFFFE');
 
+
+    /* ### Actual map code ### */
     const projection = d3.geoMercator()
       .scale(scale)
       .center([8.2310, 46.8182])
@@ -36,10 +40,12 @@ const Map:React.FC = () => {
     };
 
     d3.json('https://raw.githubusercontent.com/wojwozniak/maps/main/public/ch-cantons.geojson')
-        .then(ready)
-        .catch((error) => {
-            throw error;
-        });
+      .then(ready)
+      .catch((error) => {
+        throw error;
+    });
+    /* ### End of actual map code ### */
+
 
 
     // ### Resize ###
