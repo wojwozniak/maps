35c5cfb64da24e6bdde14cef54bf8160220b7700
Wojtek <pulchnymamut@gmail.com>
2023-08-18 19:35:55 +0200

    PickDataMenu updates

--- src/components/PickDataMenu.tsx
+++ src/components/PickDataMenu.tsx
@@ -15,8 +15,15 @@ const PickDataMenu:React.FC<PickDataMenuProps> = ({updateDataset, openPopup}) =>
   return (
     <div className='pick__data__menu' style={openPopup ? openStyle : emptyStyle}>
         <div className="content">
-            <h2>Welcome to Interactive Swiss Data Viewer!</h2>
-            <h4>Choose dataset to Explore:</h4>
+            <h2 id="title">Interactive D3.js Maps</h2>
+            <h4 id="subtitle">This website contains series of interactive maps with data about Switzerland</h4>
+            <p>Website created with d3.js, react and statistical data from x</p>
+            <div>
+                <p>Select dataset from list</p>
+                <ol>
+                    <li>aaa</li>
+                </ol>
+            </div>
         </div>
     </div>
   )
