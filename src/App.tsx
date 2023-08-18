2ffac6ec97cc28e822d1acfec79729a45b6d8f02
Wojtek <pulchnymamut@gmail.com>
2023-08-18 23:42:39 +0200

    save lang

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import Title from './components/Title';
 i18n.use(Backend).use(LanguageDetector).init({
   fallbackLng: 'en',
   detection: {
-    order: ['navigator'],
+    order: ['localStorage', 'navigator'],
   },
   interpolation: {
     escapeValue: false,
