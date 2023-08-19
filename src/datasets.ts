6a32c30c9c16d0367c38f33179f4e01d1c0120a9
Wojtek <pulchnymamut@gmail.com>
2023-08-19 19:24:05 +0200

    update datasets

--- src/datasets.ts
+++ src/datasets.ts
@@ -9,23 +9,23 @@ export interface Dataset {
 export const DatasetList: Dataset[] = [
     { 
         name: "2015-population",
-        labelEN: "Total canton population in 2015",
-        labelPL: "Całkowita liczba ludności w kantonach w 2015",
-        labelDE: "Gesamtbevölkerung der Kantone im Jahr 2015",
+        labelEN: "Total population in 2015",
+        labelPL: "Całkowita liczba ludności w 2015",
+        labelDE: "Gesamtbevölkerung im Jahr 2015",
         link: 'https://raw.githubusercontent.com/wojwozniak/maps/main/public/statistics/2015-population.json'
     },
     { 
         name: "2020-gdp-per-capita",
-        labelEN: "GDP per Capita in Switzerland in 2020",
-        labelPL: "PKB na mieszkańca w Szwajcarii w 2020",
-        labelDE: "BIP pro Kopf in der Schweiz im Jahr 2020",
+        labelEN: "GDP per Capita in 2020",
+        labelPL: "PKB na mieszkańca w 2020",
+        labelDE: "BIP pro Kopf im Jahr 2020",
         link: 'https://raw.githubusercontent.com/wojwozniak/maps/main/public/statistics/2020-gdp-per-capita.json'
     },
     { 
         name: "pop-density",
-        labelEN: "Population density in Switzerland in 2015",
-        labelPL: "Gęstość zaludnienia w Szwajcarii w 2015",
-        labelDE: "Bevölkerungsdichte in der Schweiz im Jahr 2015",
+        labelEN: "Population density in 2015",
+        labelPL: "Gęstość zaludnienia w 2015",
+        labelDE: "Bevölkerungsdichte im Jahr 2015",
         link: 'https://raw.githubusercontent.com/wojwozniak/maps/main/public/statistics/population-density.json'
     }
 ];
