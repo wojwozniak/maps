cd3a11bca6094583acb16c82d2ca9a1f1343dc2f
Wojtek <pulchnymamut@gmail.com>
2023-08-22 18:31:50 +0200

    delete bad looking dataset

--- src/datasets.ts
+++ src/datasets.ts
@@ -19,12 +19,6 @@ export const DatasetList: Dataset[] = [
         map: "https://raw.githubusercontent.com/wojwozniak/maps/main/public/ch-cantons.geojson"
     },
     { 
-        name: "pop-density",
-        label: "Population density in 2015",
-        link: 'https://raw.githubusercontent.com/wojwozniak/maps/main/public/statistics/population-density.json',
-        map: "https://raw.githubusercontent.com/wojwozniak/maps/main/public/ch-cantons.geojson"
-    },
-    { 
         name: "2020-gdp",
         label: "GDP in 2020 in millions CHF",
         link: 'https://raw.githubusercontent.com/wojwozniak/maps/main/public/statistics/2020-total-gdp.json',
